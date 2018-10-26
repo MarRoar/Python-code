a190bbd58bdaa87f9449bc56ce2d073feb784224
Marui <Mar_person@163.com>
2018-10-26 20:24:08 +0800

    django

--- 00-itecast/02-core-python/03-generate/01-fib.py
+++ 00-itecast/02-core-python/03-generate/01-fib.py
@@ -1,21 +1,19 @@
 
-def fib():
+def fib(n):
     a, b = 0, 1
-    for x in range(5):
+    for x in range(n):
         yield b
         a, b = b, a + b
 
-r = fib()
-print(r) #<function fib at 0x00884228> 可以看到结果是生成器
-
-print(next(r)) # 通过next()函数可以打印出结果
-print(next(r))
-print(next(r))
-print(next(r))
-print(next(r))
+r = fib(10)
+# print(r) #<function fib at 0x00884228> 可以看到结果是生成器
+#
+# print(next(r)) # 通过next()函数可以打印出结果
+# print(next(r))
+# print(next(r))
+# print(next(r))
+# print(next(r))
 
 print("-----------------")
 for x in r:
     print(x)
-
-
