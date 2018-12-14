3fb8200f8bca118a199104128d18fb279785ba2b
Marui <Mar_person@163.com>
2018-12-14 18:30:38 +0800

    scrapy-redis

--- Python-pachong/t.py
+++ Python-pachong/t.py
@@ -1,6 +1,20 @@
-import os
+import pymongo
 
-# os.mkdir('test') 
-os.rmdir('./test')
+client = pymongo.MongoClient('localhost', 27017)
 
-print os.path.exists('test')
+db = client.mar
+
+# collection is stu
+# stu = db.stu
+
+# s1 = {'name': 'aa', 'gender': 2}
+# s1_id = stu.insert_one(s1).inserted_id
+
+
+list1 = db.list1
+s2 = list1.find()
+
+for cur in list1.find():
+	print cur
+
+
